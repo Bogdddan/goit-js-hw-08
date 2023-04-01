afd9bbd066f40d4eb706bb6292eb152dc47c76dd
Bogdddan <bogdankrajnik95@gmail.com>
2023-04-01 18:28:58 +0300

    second homew

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player(document.getElementById('vimeo-player'));
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle((event) => {
+    const currentTime = event.seconds;
+    localStorage.setItem(STORAGE_KEY, currentTime);
+}, 1000));
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
