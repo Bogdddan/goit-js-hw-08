734ea77563b7099cab6d59539c821d524641b50e
Bogdddan <bogdankrajnik95@gmail.com>
2023-04-02 17:09:50 +0300

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+// const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+const button = document.querySelector('.buttonre');
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+
+button.addEventListener('submit' , onFormSubmit);
+messageTextarea.addEventListener(STORAGE_KEY , onTextarea);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+
+function onTextarea(event) {
+    const message = event.currentTarget.value;
+
+    localStorage.setItem(STORAGE_KEY, message);
+}
+
+
+fillTextarea();
+
+function fillTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if(savedMessage){
+        messageTextarea.value = savedMessage;
+    }
+};
